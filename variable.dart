24a33f6d17fba5d5809026cad1456304f4deb1d4
sanji2207 <saepullohepul565@gmail.com>
2023-01-14 23:02:58 +0700

    14-01-2023

--- variable.dart
+++ variable.dart
@@ -1,7 +1,11 @@
 void main() {
   String nama = "Epul Saepuloh";
   num a = 100;
+  final jenisKelamin = 'Laki-laki';
+  const negara = 'Indonesia';
 
   print(nama);
   print(a);
+  print(jenisKelamin);
+  print(negara);
 }
