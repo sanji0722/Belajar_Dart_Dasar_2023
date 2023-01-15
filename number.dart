aec549846075afd934f4778430122b44335280ba
sanji2207 <saepullohepul565@gmail.com>
2023-01-15 09:20:26 +0700

    Tipe Data Number

---
+++ number.dart
@@ -0,0 +1,9 @@
+void main() {
+  num a = 100;
+  int b = 12;
+  double c = 1.3;
+
+  print('Tipe data num $a');
+  print('Tipe data int $b');
+  print('Tipe data double $c');
+}
--- variable.dart
+++ variable.dart
@@ -4,8 +4,23 @@ void main() {
   final jenisKelamin = 'Laki-laki';
   const negara = 'Indonesia';
 
+  final array1 = [1, 1, 1, 1];
+  const array2 = [2, 2, 2, 2];
+
+  array1[1] = 100;
+
   print(nama);
   print(a);
   print(jenisKelamin);
   print(negara);
+
+  late var kata = getValue();
+  print('hallo');
+
+  print(kata);
+}
+
+String getValue() {
+  print("memanggil get value");
+  return "get value";
 }
