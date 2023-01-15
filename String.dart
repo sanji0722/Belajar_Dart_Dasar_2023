a32d02379f7ca252bb0fae7c5c0ba1efad4d2a58
sanji2207 <saepullohepul565@gmail.com>
2023-01-15 22:34:57 +0700

    String

---
+++ String.dart
@@ -0,0 +1,16 @@
+void main() {
+  String firstName = 'Epul';
+  String lastName = 'Saepuloh';
+
+  //String interpolation
+
+  print("ini adalah String interpolation $firstName");
+  print(lastName);
+
+  var nama = firstName + lastName;
+
+  print(nama);
+
+  var names = 'Toni' 'Tatang' 'Sholeh';
+  print(names);
+}
