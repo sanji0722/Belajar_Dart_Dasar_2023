b4117b2e68d109c14cd4d50652ab995401c7d346
sanji2207 <saepullohepul565@gmail.com>
2023-01-16 17:45:21 +0700

    dynamic konversi data

---
+++ dynamic.dart
@@ -0,0 +1,7 @@
+void main() {
+  dynamic variable = 100;
+
+  variable = "Hello";
+
+  print(variable);
+}
---
+++ konversi_tipedata.dart
@@ -0,0 +1,21 @@
+void main() {
+  var dataString = '100';
+
+  print(dataString);
+
+  var dataInt = int.parse(dataString);
+  print(dataInt);
+  var dataDoble = double.parse(dataString);
+  print(dataDoble);
+
+  var angka = 23;
+  print(angka);
+  var doubleangkat = angka.toDouble();
+
+  print(doubleangkat);
+
+  var contoh = 'true';
+
+  var ini = contoh == 'true';
+  print(ini);
+}
